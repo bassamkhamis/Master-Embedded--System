f5667c5b6cd5eef1cb3735f289530976e282c034
Bassam <bassammansour96@gmail.com>
2021-10-25 13:18:50 +0200

    code factorial use Recusion fun

---
+++ Cprogramming/assignments/lec5-ass/EXR1/main.c
@@ -0,0 +1,49 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 13, 2021
+ *      Author: bassam
+ */
+
+#include <stdio.h>
+
+int funTocheck(int num){
+
+
+	int count ,flag=1;
+
+	for (count=2; count<num; count++){
+
+		if (num%count == 0){
+			flag=0;
+			break;
+		}
+	}
+
+	return flag;
+}
+
+int main(void){
+
+
+	int num1,num2,count,flag;
+
+
+	printf("enter two number (interval)");
+	fflush(stdin);fflush(stdout);
+	scanf("%d%d",&num1,&num2);
+	printf("prime number btw %d and %d are\n",num1,num2);
+for (count=num1+1; count<num2; count++){
+	flag=funTocheck(count);
+	if (flag){
+		printf("%d ",count);
+	}
+}
+
+
+
+}
+
+
+
+
