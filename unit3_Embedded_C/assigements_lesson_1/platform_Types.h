fd6f00939935f1c34375ca68357474f1bed6cb44
Bassam <bassammansour96@gmail.com>
2021-12-14 21:09:51 +0200

    tyoedef data types

---
+++ unit3_Embedded_C/assigements_lesson_1/platform_Types.h
@@ -0,0 +1,25 @@
+/***********************************************************************************/
+/* Auther  : Bassam                                                                 */
+/* Date    :14 Dec 2021                                                             */
+/* Version :v01                                                                     */
+/***********************************************************************************/ 
+
+
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+typedef unsigned char                uint8_t
+typedef unsigned short int           uint16_t
+typedef unsigned long int            uint32_t
+typedef unsigned long long int       uint64_t
+
+typedef volatile unsigned char                vuint8_t
+typedef volatile unsigned short int           vuint16_t
+typedef volatile unsigned long int            vuint32_t
+typedef volatile unsigned long long int       vuint64_t
+
+
+
+
+
+#endif 
